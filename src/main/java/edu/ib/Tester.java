20e9382ba25d321c0cb6b1aa670e786d0c7370b8
Miroslaw Latka <mirek@Miroslaws-MacBook-Air.local>
2021-12-03 07:13:02 +0100

    Initial commit

---
+++ src/main/java/edu/ib/Tester.java
@@ -0,0 +1,83 @@
+package edu.ib;
+
+import java.sql.*;
+public class Tester {
+    public static void main(String[] args) {
+
+        StringBuilder url=new StringBuilder();
+        url.append("jdbc:mysql://");
+        url.append("localhost:3306/");
+        url.append("dbMovie?");
+        url.append("useUnicode=true&characterEncoding=utf-8");
+        url.append("&user=root");
+        url.append("&password=eduibtest");
+        url.append("&serverTimeZone=CET");
+
+        String urlConnection=url.toString();
+
+
+        try{
+
+            Connection connection=DriverManager.getConnection(urlConnection);
+
+            PreparedStatement showTableStm=connection.prepareStatement("SHOW TABLES");
+            ResultSet rsTables=showTableStm.executeQuery();
+
+            while(rsTables.next()){
+                String answer=rsTables.getString(1);
+                System.out.println(answer);
+            }
+            showTableStm.close();
+            rsTables.close();
+
+
+           PreparedStatement selectAllStm=connection.prepareStatement(
+                   "SELECT * FROM table_mcharacters;");
+           ResultSet rsTableContent=selectAllStm.executeQuery();
+//            while(rsTableContent.next()){
+//                String answer=rsTableContent.getString("last");
+//                System.out.println(answer);
+//            }
+            printResultSet(rsTableContent);
+
+            String insertString="INSERT INTO table_mcharacters" +
+                    "(id,first,last,age)"+
+                    "VALUES"+
+                    "(200,'John','Doe',23);";
+            PreparedStatement insertStm=connection.prepareStatement(insertString);
+            int rowAffected=insertStm.executeUpdate();
+
+            System.out.println("Affected rows= " + rowAffected);
+
+            PreparedStatement insertCheckStm=connection.prepareStatement(
+                    "SELECT * FROM table_mcharacters;");
+            ResultSet rsTableCheck=insertCheckStm.executeQuery();
+            printResultSet(rsTableCheck);
+
+            String deleteString="DELETE FROM table_mcharacters WHERE last='Doe';";
+            PreparedStatement deleteStm=connection.prepareStatement(deleteString);
+            rowAffected=deleteStm.executeUpdate();
+            System.out.println("Affected rows= " + rowAffected);
+
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static  void printResultSet(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData rsmd=resultSet.getMetaData();
+        int columnCount=rsmd.getColumnCount();
+        while(resultSet.next()){
+
+            for(int i=1; i<=columnCount; i++){
+                String colVal=resultSet.getString(i);
+                System.out.print(colVal + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
